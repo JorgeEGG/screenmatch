6c39a0f8e3ff232e1fa05691412fe9ede444929f
Jorge E. <jkgonzalezg@hotmail.com>
2025-12-10 20:35:23 -0500

    Se modifica archivo PrincipalConBusqueda y se  adiciona gson.jar

--- src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -1,15 +1,33 @@
 package com.aluracursos.screenmatch.principal;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
 public class PrincipalConBusqueda {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("\nIngrese el nombre de la pelicula: ");
+        var busqueda = teclado.next();
+
+        String direccion = "https://www.omdbapi.com/?t="+busqueda+"&apikey=53a418d1";
+
+
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://foo.com/"))
+                .uri(URI.create(direccion))
                 .build();
+
+        HttpResponse<String> response = client
+                .send(request, BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
     }
 }
